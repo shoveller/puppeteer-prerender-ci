5b5b8db72a2789e17011e302b40d6b3c508d94fc
cinos81 <cinos81@gmail.com>
2025-05-17 21:04:45 +0900

    feat: update Vite configuration to include Puppeteer executable path and arguments

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import prerender from "@prerenderer/rollup-plugin";
 import PuppeteerRenderer from "@prerenderer/renderer-puppeteer";
+import path from "node:path";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -18,6 +19,10 @@ export default defineConfig({
       rendererOptions: {
         maxConcurrentRoutes: 1,
         renderAfterTime: 500,
+        executablePath:
+          process.env.PUPPETEER_EXECUTABLE_PATH ||
+          path.join(process.cwd(), "chrome-linux", "chrome"),
+        args: ["--no-sandbox", "--disable-setuid-sandbox"],
       },
       postProcess(renderedRoute) {
         // 빌드 결과물에 http 또는 127.0.0.1, localhost 가 명시되어 있다면 다른 값으로 치환한다.
